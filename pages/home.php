1446bba4299bb876adb79b47ae57316e4595463c
Anthony BIZIEN <abizien@eurocomputer.priv>
2020-01-15 17:17:10 +0100

    login second part and template home

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,15 @@
 <?php
 require "./vendor/autoload.php";
-use App\twigloader;
-?>
-<h1>Home Works</h1>
+require "./App/twigloader.php";
+session_start();
+
+ //URL bloqué si pas de connexion :
+ if (empty($_SESSION['user'])) {
+    header('location: login');
+ }
+
+ $user = $_SESSION['user'];
+
+// Donnée transmise au template : 
+echo $twig->render('home.twig',['user'=>'$user']);
+var_dump($user);
--- pages/login.php
+++ pages/login.php
@@ -1,9 +1,14 @@
 <?php
  require "./vendor/autoload.php";
  require "./App/twigloader.php";
+ session_start();
+    //si une session est ouverte :
+    if (!empty($_SESSION['user'])) {
+        header('location: home');
+    }
 
     //si la connexion n'existe pas : 
-    if (!isset($Database)) {
+    if (empty($Database)) {
 
        // Connexion à la base de donnée et à la table Utilisateur :
         $Database = new App\Database('devisrecode');
@@ -19,15 +24,15 @@
 
         // verification des concordances en base de donnée : 
        $login = $Users->login($_POST['login'],$_POST['pass']);
-        
+            
        switch ($login) {
            // si erreur envoi info au template : 
            case false:
                echo $twig->render('login.twig',['login'=>'$login']);
-               echo 'raté';
                break;
-           // sinon je dirige l'utilisateur vers "home" et déclenche sa session : 
+           // sinon redirection de l'utilisateur vers "home"  : 
            default:
+               $_SESSION['user'] = $login ;
                header('location: home');
                break;
        }
