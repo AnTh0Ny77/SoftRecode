79b434edae7c578d29f1252cfbac43b8c11fbac0
Anthony BIZIEN <abizien@eurocomputer.priv>
2020-02-03 17:12:39 +0100

    javascript traitement

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 //    initialisation table client : 
-    var tableClient = $('#client').DataTable({
+    let tableClient = $('#client').DataTable({
         "paging": true,
         "info":   true,
         retrieve: true,
@@ -19,7 +19,7 @@ $(document).ready(function() {
     });
 
     //initialisation table contact  
-    var tableContact = $("#contactTable").DataTable({
+    let tableContact = $("#contactTable").DataTable({
         "paging": false,
         "info":   true,
         retrieve: true,
@@ -29,34 +29,73 @@ $(document).ready(function() {
         
     // fonction selection du client  : 
     $('#client tbody').on('click', 'tr', function () {
-        var data = tableClient.row( this ).data();
+        let data = tableClient.row( this ).data();
         $("#choixClient").val(data[0]);
         $("#formSelectClient").submit();
     });
 
     // fonction selection du contact : 
     $('#contactTable tbody').on('click','tr', function(){
-        var text = tableContact.row( this ).data();
+        let text = tableContact.row( this ).data();
         $("#choixContact").val(text[0]);
         $("#formSelectContact").submit();
     })
     
     // Programme d'ajout de ligne dans le devis : 
     //traitement du formulaire : 
-    var referenceStricte ;
+    let referenceStricte ;
     $('#choixDesignation option').on('click', function(){
-        var referenceStricte = $('#referenceS').val($(this).text());
+        let referenceStricte = $('#referenceS').val($(this).text());
     });
     // extension de garantie : 
-    var xtendMois ; 
-    $("#xtendMois ").change(function(){
-        var xtendMois = $(this).val();
-    })
-    var xtendPrix;
+    let xtendMois ; 
+    let xtendPrix;
+    let ul;
+    let li;
+    let i;
     $("#xtendGr").on('click', function(){
-        xtendMois =  $("#xtendMois").$(this).children("option:selected").val();
-        
-        console.log(xtendMois);
+        if (!$("#xtendPrice").val() && !$("#xtendPrice").val()) {
+            $("#xtendPrice").addClass('alert alert-danger');
+        }else {
+            $("#xtendPrice").removeClass('alert alert-danger');
+            xtendPrix = $('#xtendPrice').val();  
+            xtendMois = $('#xtendMois').val(); 
+            let xtendCouple = [ xtendMois ,  xtendPrix ]; 
+             ul = $("#xtendList")
+             li =  $('<li></li>').text(xtendCouple[0] + " mois " + xtendCouple[1] + "€ H.T ").addClass('list-group-item col-4 d-flex justify-content-between align-items-center').appendTo(ul);
+             i =  $('<i></i>').addClass('fal fa-trash-alt btn btn-link').appendTo(li);
+            xtendCouple = [];  
+        }
+        $(i).on('click', function(){
+            $(this).parent().remove();    
+        })
     })
+
+    //ajout d'une ligne de devis :
+    $("#addRow").on('click', function(){
+        let row = []; 
+        let prestation = $("#prestationChoix").val();
+        row.push(prestation);
+        let designation = $("#referenceS").val();
+        row.push(designation);
+        let etat = $("#etatRow").val();
+        row.push(etat);
+        let garantie = $("#garantieRow").val();
+        row.push( garantie);
+        let quantite =  $("#quantiteRow").val();
+        row.push( quantite);
+        let prix = $("#prixRow").val();
+        row.push( prix);
+        let comClient = $("#comClient").val();
+        row.push(comClient);
+        let comInterne = $("#comInterne").val();
+        row.push(comInterne);
+        let xtend = [];
+        
+        console.log(row);
+    })
+  
+        
+   
     
 } );
