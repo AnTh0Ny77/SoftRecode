72d74cf9a63d69997f8294588852e997f72e07dc
Anthony BIZIEN <abizien@eurocomputer.priv>
2020-02-04 17:43:32 +0100

    javascript modal to datat & datremove

--- public/js/client.js
+++ public/js/client.js
@@ -10,12 +10,13 @@ $(document).ready(function() {
     });
 
    // initialisation table devis : 
-    $('#Devis').DataTable({
+   let devisTable =  $('#Devis').DataTable({
         "paging": false,
         "info":   false,
         "searching": false,
         deferRender:  true,
-        rowReorder: true
+        rowReorder: true,
+        
     });
 
     //initialisation table contact  
@@ -50,30 +51,39 @@ $(document).ready(function() {
     // extension de garantie : 
     let xtendMois ; 
     let xtendPrix;
-    let ul;
-    let li;
-    let i;
+    let xtendArray = [];
+   
     $("#xtendGr").on('click', function(){
         if (!$("#xtendPrice").val() && !$("#xtendPrice").val()) {
             $("#xtendPrice").addClass('alert alert-danger');
         }else {
             $("#xtendPrice").removeClass('alert alert-danger');
+            $("#xtendList").empty();
             xtendPrix = $('#xtendPrice').val();  
             xtendMois = $('#xtendMois').val(); 
-            let xtendCouple = [ xtendMois ,  xtendPrix ]; 
-             ul = $("#xtendList")
-             li =  $('<li></li>').text(xtendCouple[0] + " mois " + xtendCouple[1] + "€ H.T ").addClass('list-group-item col-4 d-flex justify-content-between align-items-center').appendTo(ul);
-             i =  $('<i></i>').addClass('fal fa-trash-alt btn btn-link').appendTo(li);
-            xtendCouple = [];  
-        }
-        $(i).on('click', function(){
-            $(this).parent().remove();    
-        })
+            let  xtendCouple = [ xtendMois , xtendPrix ]; 
+            xtendArray.push(xtendCouple);
+            for (let index = 0; index < xtendArray.length; index++) {
+                let ul = $("#xtendList");
+                let li =  $('<li></li>').text(xtendArray[index][0] + " mois " + xtendArray[index][1] + "€ H.T ").addClass('list-group-item col-4 d-flex justify-content-between align-items-center').appendTo(ul);
+                let i =  $('<i></i>').addClass('fal fa-trash-alt btn btn-link').appendTo(li);
+                $(i).on('click', function(){
+                    $(this).parent().remove();    
+                    xtendArray[index].splice(0,3);
+                    console.log(xtendArray);
+                })
+            }
+             xtendCouple = [];
+        }   
     })
-
+       
     //ajout d'une ligne de devis :
+    counter = 1 ;
     $("#addRow").on('click', function(){
         let row = []; 
+        let modify = '<i class="fal fa-edit btn"></i>';
+        row.push(counter);
+        row.push(modify);
         let prestation = $("#prestationChoix").val();
         row.push(prestation);
         let designation = $("#referenceS").val();
@@ -86,16 +96,36 @@ $(document).ready(function() {
         row.push( quantite);
         let prix = $("#prixRow").val();
         row.push( prix);
+        row.push("x " + xtendArray.length );
         let comClient = $("#comClient").val();
         row.push(comClient);
         let comInterne = $("#comInterne").val();
         row.push(comInterne);
-        let xtend = [];
-        
-        console.log(row);
+        let prixBarre = $("#barrePrice").val();
+        row.push(prixBarre);
+        let deleteButton = "<i class='fal fa-trash-alt btn deleteRow'></i>";
+        row.push(deleteButton);
+        devisTable.row.add(row).draw( false );
+        row = [];
+        xtendArray = [];
+        counter ++;
+       
+ 
     })
-  
+
+     // efface sa propre ligne : 
+     devisTable.on('click','.deleteRow',function() {
+        if ( $(this).parents('tr').hasClass('selected') ) {
+            $(this).parents('tr').removeClass('selected');
+        }
+        else {
+            devisTable.$('tr.selected').removeClass('selected');
+            $(this).parents('tr').addClass('selected');
+        }
+        devisTable.row('.selected').remove().draw( false );
+     });
+     // update sa propre ligne : 
+
         
-   
     
 } );
