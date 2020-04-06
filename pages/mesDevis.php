b6fa1c930a91aaf6403a71fbec98b07d5803efa0
Anthnoybs <anthonybs.pro@gmail.com>
2020-04-07 01:20:29 +0200

    delete devis.pdf

--- pages/mesDevis.php
+++ pages/mesDevis.php
@@ -19,8 +19,67 @@ session_start();
  $Database->DbConnect();
  $Devis = new App\Tables\Devis($Database);
  $Keyword = new App\Tables\Keyword($Database);
+ $Client = new App\Tables\Client($Database);
+ $Contact = new \App\Tables\Contact($Database);
  $listOfStatus = $Keyword->getStat();
  $devisList = [];
+
+ // Si un devis a été validé : 
+if (!empty($_POST['clientSelect'])) {
+   $devisData = json_decode($_POST["dataDevis"]);
+   $date = date("Y-m-d H:i:s");
+   $client = $Client->getOne($_POST['clientSelect']);
+  
+
+   // corrige la notice lié a l'accesion d'un non objet -> :
+   $contactId = NULL;
+   $livraisonId = NULL;
+   $livraisonContact = NULL;
+   if (!empty( $_POST['contactSelect'])) {
+     $contactId = $_POST['contactSelect'];
+     $contact = $Contact->getOne($_POST['contactSelect']);
+   }
+   if (!empty($_POST['livraisonSelect'])) {
+       $livraisonId = $_POST['livraisonSelect'];
+   }
+
+   if (!empty($_POST['contact_livraison'])) {
+       $livraisonContact = $_POST['contact_livraison'];
+   }
+   $status = 'ATN';
+
+
+   if (!empty($_POST['ModifierDevis'])) {
+       $devis = $Devis->Modify(
+       intval($_POST['ModifierDevis']),
+       $date,
+       $_SESSION['user']->id_utilisateur,
+       $_POST['clientSelect'],
+       $livraisonId,
+       $_POST['port'],
+       $contactId,
+       $_POST['globalComClient'],
+       $_POST['globalComInt'],
+       $status,
+       NULL,
+       $devisData , 
+       $livraisonContact 
+     );
+   } else {
+       $devis = $Devis->insertOne(
+           $date,
+           $_SESSION['user']->id_utilisateur,
+           $_POST['clientSelect'],
+           $livraisonId,
+           $_POST['port'],
+           $contactId,
+           $_POST['globalComClient'],
+           $_POST['globalComInt'],
+           $status,
+           NULL,
+           $devisData,
+           $livraisonContact );
+   }}
 
  if (!empty($_POST['ValiderDevis'])) {
     $Devis->updateStatus($_POST['statusRadio'],$_POST['ValiderDevis']);
