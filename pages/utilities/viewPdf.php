edd6486bfeade13b457b7245b252eea4314dbe61
Antho <anthonybs.pro@gmail.com>
2020-05-27 08:53:13 +0200

    met àjour le pdf view par rapport au visio

--- pages/utilities/viewPdf.php
+++ pages/utilities/viewPdf.php
@@ -8,6 +8,7 @@ session_start();
 $Database = new App\Database('devis');
 $Database->DbConnect();
 $Cmd = new App\Tables\Cmd($Database);
+$Contact = new \App\Tables\Contact($Database);
 $Client = new \App\Tables\Client($Database);
 
 if (empty($_SESSION['user'])) {
@@ -57,20 +58,60 @@ $garanties = $Keyword->getGaranties();
          <tr>
              <td  style="text-align: left;  width: 50% ; margin-left: 25%;"><h2>Devis <?php echo $temp->devis__id ?></h2><br><?php echo date("d-m-Y") ?><br><?php echo $_SESSION['user']->email ?><p><small>Notre offre est valable une semaine à dater du : <?php echo $formated_date ?></small></p></td>
              <td style="text-align: left; width:50%"><?php 
+             // si une societe de livraion est présente 
              if ($societeLivraison) {
-                echo "<small>facturation :</small><strong><br>";
-                echo Pdfunctions::showSociete($clientView) ." </strong> 
-                <br> <small>Livraison :</small><strong><br>";
-                echo Pdfunctions::showSociete($societeLivraison) . "</strong></td>";   
-             } 
-             else{
-                echo "<small>livraison & facturation</small><strong><br>";
-                echo Pdfunctions::showSociete($clientView)  ."</strong></td>";
 
-             } ?>
+                    if ($temp->devis__contact__id) {
+                        // si un contact est présent dans l'adresse de facturation :
+                        $contact = $Contact->getOne($temp->devis__contact__id);
+                        echo "<small>facturation : ". $contact->contact__civ . " " . $contact->contact__nom. " " . $contact->contact__prenom. "</small><strong><br>";
+                        echo Pdfunctions::showSociete($clientView) ." </strong> ";
+                    
+                        if ($temp->devis__contact_livraison) {
+                            //si un contact est présent dans l'adresse de livraison : 
+                            $contact2 = $Contact->getOne($temp->devis__contact_livraison);
+                            echo "<br> <small>Livraison : ".$contact2->contact__civ . " " . $contact2->contact__nom. " " . $contact2->contact__prenom."</small><strong><br>";
+                            echo Pdfunctions::showSociete($societeLivraison) . "</strong></td>"; 
+                        }
+                        else {
+                            // si pas de contact de livraison : 
+                            echo "<br> <small>Livraison :</small><strong><br>";
+                            echo Pdfunctions::showSociete($societeLivraison) . "</strong></td>"; 
+                        } 
+                    }
+
+                    else {
+                        echo "<small>facturation :</small><strong><br>";
+                        echo Pdfunctions::showSociete($clientView) ." </strong>" ;
+                        if ($temp->devis__contact_livraison) {
+                            $contact2 = $Contact->getOne($temp->devis__contact_livraison);
+                            echo "<br> <small>Livraison : ".$contact2->contact__civ . " " . $contact2->contact__nom. " " . $contact2->contact__prenom."</small><strong><br>";
+                            echo Pdfunctions::showSociete($societeLivraison) . "</strong></td>"; 
+                        } else {
+                            echo "<br> <small>Livraison :</small><strong><br>";
+                            echo Pdfunctions::showSociete($societeLivraison) . "</strong></td>"; 
+                        }  
+                    }  
+             } 
+
+
+
+             else{
+                if ($temp->devis__contact__id) {
+                $contact = $Contact->getOne($temp->devis__contact__id);
+                echo "<small>livraison & facturation : ". $contact->contact__civ . " " . $contact->contact__nom. " " . $contact->contact__prenom."</small><strong><br>";
+                echo Pdfunctions::showSociete($clientView)  ."</strong></td>";
+                }
+                else{
+                    echo "<small>livraison & facturation : </small><strong><br>";
+                    echo Pdfunctions::showSociete($clientView)  ."</strong></td>";
+                }
+
+             } 
+             ?>
          </tr>
      </table>
-     <table CELLSPACING=0 style="width: 760px;  margin-top: 65px;  margin-top: 65px;  ">
+     <table CELLSPACING=0 style="width: 760px;  margin-top: 65px;   ">
              <tr style=" margin-top : 50px; background-color: #dedede;  " >
                 <td style=" text-align: left;   padding-top: 4px; padding-bottom: 4px;">Prestation</td>
                 <td style=" text-align: left; padding-top: 4px; padding-bottom: 4px;">Designation</td>
@@ -102,18 +143,34 @@ $garanties = $Keyword->getGaranties();
          <tr>
          <td style="width: 250px"></td>
          <td>
-             <table CELLSPACING=0  style=" border: 1px black solid;">
-                 <tr style="background-color: #dedede;"><td style="width: 210px; text-align: left">Type de Garantie </td><td style="text-align: center; width: 85px;"><strong>Total € HT </strong></td><td style="text-align: center">Total € TTC</td></tr>
-                 <?php
-                     $totalPrice = array_sum($arrayPrice);
-                       
-                       echo  "<tr><td style='width: 210px; text-align: left'><input type='checkbox'> hors garanties</td><td style='text-align: center'><strong>  ". number_format($totalPrice,2  ,',', ' ') . " €</strong></td><td style='text-align: right'> " .number_format(Pdfunctions::ttc(floatval($totalPrice)),2 ,',', ' ')." €</td></tr>";
 
-                  
-                       $totaux = Pdfunctions::magicXtend($arrayOfDevisLigne , $garanties , array_sum($arrayPrice));
+         <?php
+         // on affiche le tableau de totaux en cas de modèle adéquate :
+            if ($temp->cmd__modele_devis == 'STT') {
+
+            $totable =  '<table CELLSPACING=0  style=" border: 1px black solid;">
+            <tr style="background-color: #dedede;">
+            <td style="width: 210px; text-align: left">Type de Garantie </td>
+            <td style="text-align: center; width: 85px;"><strong>Total € HT </strong></td>
+            <td style="text-align: center">Total € TTC</td>
+            </tr>';
+
+            $totalPrice = array_sum($arrayPrice);
+            echo $totable;
+            echo  "<tr><td style='width: 210px; text-align: left'><input type='checkbox'> hors garanties</td>
+            <td style='text-align: center'><strong>  ". number_format($totalPrice,2  ,',', ' ') . " €</strong></td>
+            <td style='text-align: right'> " .number_format(Pdfunctions::ttc(floatval($totalPrice)),2 ,',', ' ')." €</td>
+            </tr>";
+            $totaux = Pdfunctions::magicXtend($arrayOfDevisLigne , $garanties , array_sum($arrayPrice));
+            echo '</table>';
+            }
+            
+                
+                 
+                    
                     
                  ?>
-             </table>
+             
          </td>
          </tr>
      </table>
