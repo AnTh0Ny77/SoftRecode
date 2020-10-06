237f9a1bdc7c7a49a2d94f0de72dccb713a67072
Antho <anthonybs.pro@gmail.com>
2020-10-06 14:34:18 +0200

    security

--- pages/noAccess.php
+++ pages/noAccess.php
@@ -1,5 +1,10 @@
 <?php
  session_start();
+
+ if (empty($_SESSION['user'])) 
+ {
+   header('location: login');
+ }
  require "./App/twigloader.php";
  echo $twig->render('noAccess.twig', [
     'user'=>$_SESSION['user']
