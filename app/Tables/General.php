f5ddefa577829347f08ba248b3d9a370ab59d340
Antho <anthonybs.pro@gmail.com>
2020-10-06 16:17:05 +0200

    export_TNT

--- app/Tables/General.php
+++ app/Tables/General.php
@@ -110,7 +110,7 @@ class General extends Table
     $commande->client__livraison__adr1 = $this->replaceSpecialChar($commande->client__livraison__adr1);
     $commande->client__livraison__adr2 = $this->replaceSpecialChar($commande->client__livraison__adr2);
 
-    $responseText = ';02008066;'.$commande->cmd__date_envoi.';;;'.$paquets.';'.$poids.';'.$paquets.';'.$poids.';E;;'.$commande->client__livraison_societe.';'.$commande->client__livraison__adr1.';'.$commande->client__livraison__adr2.';'.$commande->client__livraison_cp.';'.$commande->client__livraison_ville.';;;'.$commande->nom__livraison.';'.$commande->nom__livraison.';;'.$commande->fixe__livraison.';'.$commande->client__tel.';'.$commande->mail__livraison.';'.$recodeRaison.';'.$recodeAdresse1.';'.$recodeAdresse2.';'.$recodeCP.';'.$recodeCommune.';'.$recodeTel.';;'.$recodeRaison.';'.$recodeAdresse1.';'.$recodeAdresse2.';'.$recodeCP.';'.$recodeCommune.';'.$recodeTel.';J;;;;;;;;;;
+    $responseText = ';02008066;'.$commande->cmd__date_envoi.';;;'.$paquets.';'.$poids.';'.$paquets.';'.$poids.';E;;'.$commande->client__livraison_societe.';'.$commande->client__livraison__adr1.';'.$commande->client__livraison__adr2.';'.$commande->client__livraison_cp.';'.$commande->client__livraison_ville.';;;'.$commande->nom__livraison.';'.$commande->nom__livraison.';;'.$commande->client__tel.';'.$commande->client__tel.';'.$commande->mail__livraison.';'.$recodeRaison.';'.$recodeAdresse1.';'.$recodeAdresse2.';'.$recodeCP.';'.$recodeCommune.';'.$recodeTel.';;'.$recodeRaison.';'.$recodeAdresse1.';'.$recodeAdresse2.';'.$recodeCP.';'.$recodeCommune.';'.$recodeTel.';J;;;;;;;;;;
 ';
     
     return $responseText;
