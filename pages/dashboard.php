6a995d359f84f917107b91be9b3d857d5363394c
Antho <anthonybs.pro@gmail.com>
2020-10-07 10:05:10 +0200

    dashboard

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,6 +1,10 @@
 <?php
  session_start();
  require "./App/twigloader.php";
+ if (empty($_SESSION['user'])) 
+ {
+   header('location: login');
+ }
  echo $twig->render('dashboard.twig', [
     'user'=>$_SESSION['user']
  ]);
