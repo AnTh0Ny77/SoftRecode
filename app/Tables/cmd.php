0dad2e9763b1273a94fa2b7a01e82adcd8f7d8f4
Antho <anthonybs.pro@gmail.com>
2020-10-08 09:34:56 +0200

    facture reliquat

--- app/Tables/cmd.php
+++ app/Tables/cmd.php
@@ -801,110 +801,188 @@ public function FactureReliquat($cmd)
   }
 $command = $this->getById(intval($idReliquat));
 $commandLignes = $this->devisLigne($idReliquat);
+$Client = new Client($this->Db);
+$Contact = new Contact($this->Db);
+$clientView = $Client->getOne($command->client__id);
+
+
+    $societeLivraison = false ;
+
+    if ($command->devis__id_client_livraison) 
+    {
+        $societeLivraison = $Client->getOne($command->devis__id_client_livraison);
+    }
 $dateTemp = new DateTime($command->cmd__date_cmd);
  //cree une variable pour la date de commande du devis
  $date_time = new DateTime( $command->cmd__date_cmd);
  //formate la date pour l'utilisateur:
  $formated_date = $date_time->format('d/m/Y');
-ob_start();
-?>
-<style type="text/css">
-      strong{ color:#000;}
-      h3{ color:#666666;}
-      h2{ color:#3b3b3b;}
-      table{
-        font-size:13; font-style: normal; font-variant: normal; 
-       border-collapse:separate; 
-       border-spacing: 0 15px; 
-         }  
- </style>
-
-<page backtop="10mm" backleft="15mm" backright="15mm">
-     <table style="width: 100%;">
-         <tr>
-             <td style="text-align: left;  width: 50%"><img  style=" width:60mm" src="public/img/recodeDevis.png"/></td>
-             <td style="text-align: left; width:50%"><h3>Reparation-Location-Vente</h3>imprimantes- lecteurs codes-barres<br>
-             <a>www.recode.fr</a><br><br>
-             <br><strong>REF CLIENT :<?php echo $command->client__id ?></strong></td>
-             </tr>
-             <tr>
-             <td  style="text-align: left;  width: 50% ; margin-left: 25%;"><h4>Fiche De travail -  <?php echo $command->devis__id ?></h4>
-             <barcode dimension="1D" type="C128" label="none" value="<?php echo $command->devis__id ?>" style="width:40mm; height:8mm; color: #3b3b3b; font-size: 4mm"></barcode><br>
-
-             <small>Commandé le : <?php echo $formated_date ?></small><br>
-             Vendeur :<?php echo  $_SESSION['user']->log_nec ?> </td>
-             <td style="text-align: left; width:50%"><strong>
-             <?php echo $command->client__societe ?><br><?php echo $command->client__adr1 ?><br><?php if (!empty($command->client__adr2)) {
-                 echo $command->client__adr2; } ?>
-             <br>
-             <?php echo $command->client__cp ." ". $command->client__ville ?></strong><br>
-             <?php echo $command->contact__nom . " " . $command->contact__prenom   ?> <br>
-             <strong>
-             <?php
-             if (!empty($command->cmd__code_cmd_client)) 
-             {
-              echo $command->cmd__code_cmd_client;
-             } 
-             ?>
-             </strong>
-            </td>
-         </tr>
-     </table>
-
-
-     <table CELLSPACING=0 style="width: 700px;  margin-top: 80px; ">
-             <tr style=" margin-top : 50px; background-color: #dedede;">
-                <td style="width: 22%; text-align: left;">Presta<br>Type<br>Gar.</td>
-                <td style="width: 57%; text-align: left">Ref Tech<br>Désignation Client<br>Complement techniques</td>
-                <td style="text-align: right; width: 12%"><strong>CMD</strong><br>Livr</td>
-             </tr> 
-             <?php
-             foreach ($commandLignes as $item) {
-                if($item->cmdl__garantie_option > $item->devl__mois_garantie) 
-                {
-                  $temp = $item->cmdl__garantie_option ;
-                } else {  $temp = $item->devl__mois_garantie;}
-
-               
-
-                echo "<tr style='font-size: 85%;>
-                        <td style='border-bottom: 1px #ccc solid'> ". $item->prestaLib." <br> " .$item->kw__lib ." <br> " . $temp ." mois</td>
-                        <td style='border-bottom: 1px #ccc solid; width: 55%;'> "
-                            .$item->famille__lib. " " . $item->marque . " " .$item->modele. " ". $item->devl__modele . 
-                            "<br> <small>désignation sur le devis:</small> " . $item->devl__designation ." <br>" .$item->devl__note_interne .
-                        "</td>
-                         <td style='border-bottom: 1px #ccc solid; text-align: right'><strong> "  . $item->devl_quantite. " </strong></td>
-                      </tr>";
+ ob_start();
+ ?>
+ <style type="text/css">
+       strong{ color:#000;}
+       h3{ color:#666666;}
+       h2{ color:#3b3b3b;}
+       table{
+         font-size:13; font-style: normal; font-variant: normal; 
+        border-collapse:separate; 
+        border-spacing: 0 15px; 
+          }  
+  </style>
+ 
+ <page backtop="10mm" backleft="5mm" backright="5mm">
+      <table style="width: 100%;">
+          <tr>
+              <td style="text-align: left;  width: 50%"><img  style=" width:60mm" src="public/img/recodeDevis.png"/></td>
+              <td style="text-align: left; width:50%"><h3>Reparation-Location-Vente</h3>imprimantes- lecteurs codes-barres<br>
+              <a>www.recode.fr</a><br><br>
+              <br></td>
+              </tr>
+              <tr>
+              <td  style="text-align: left;  width: 50% ; margin-left: 25%;"><h4>Fiche De travail -  <?php echo $command->devis__id ?></h4>
+              <barcode dimension="1D" type="C128" label="none" value="<?php echo $command->devis__id ?>" style="width:40mm; height:8mm; color: #3b3b3b; font-size: 4mm"></barcode><br>
+ 
+              <small>Commandé le : <?php echo $formated_date ?></small><br>
+              Vendeur :<?php echo  $_SESSION['user']->log_nec ?> </td>
+              <td style="text-align: left; width:50%"><strong><?php 
+               if ($societeLivraison) 
+               {
+ 
+                 if ($command->devis__contact__id) {
+                     // si un contact est présent dans l'adresse de facturation :
+                     $contact = $Contact->getOne($command->devis__contact__id);
+                     echo "<small>facturation : ". $contact->contact__civ . " " . $contact->contact__nom. " " . $contact->contact__prenom. "</small><strong><br>";
+                     echo Pdfunctions::showSociete($clientView) ." </strong> ";
+                 
+                     if ($command->devis__contact_livraison) {
+                         //si un contact est présent dans l'adresse de livraison : 
+                         $contact2 = $Contact->getOne($command->devis__contact_livraison);
+                         echo "<br> <small>livraison : ".$contact2->contact__civ . " " . $contact2->contact__nom. " " . $contact2->contact__prenom."</small><strong><br>";
+                         echo Pdfunctions::showSociete($societeLivraison) . "</strong>"; 
+                     }
+                     else {
+                         // si pas de contact de livraison : 
+                         echo "<br> <small>livraison :</small><strong><br>";
+                         echo Pdfunctions::showSociete($societeLivraison) . "</strong>"; 
+                     } 
+                 }
+ 
+                 else {
+                     echo "<small>facturation :</small><strong><br>";
+                     echo Pdfunctions::showSociete($clientView) ." </strong>" ;
+                     if ($command->devis__contact_livraison) {
+                         $contact2 = $Contact->getOne($command->devis__contact_livraison);
+                         echo "<br> <small>livraison : ".$contact2->contact__civ . " " . $contact2->contact__nom. " " . $contact2->contact__prenom."</small><strong><br>";
+                         echo Pdfunctions::showSociete($societeLivraison) . "</strong>"; 
+                     } else {
+                         echo "<br> <small>livraison :</small><strong><br>";
+                         echo Pdfunctions::showSociete($societeLivraison) . "</strong>"; 
+                     }  
+                 }  
+          } 
+ 
+ 
+ 
+          else{
+             if ($command->devis__contact__id) {
+             $contact = $Contact->getOne($command->devis__contact__id);
+             echo "<small>livraison & facturation : ". $contact->contact__civ . " " . $contact->contact__nom. " " . $contact->contact__prenom."</small><strong><br>";
+             echo Pdfunctions::showSociete($clientView)  ."</strong>";
              }
-             ?>
-     </table> 
-     <table style=" margin-top: 200px; width: 100%">
-             <tr style=" margin-top: 200px; width: 100%"><td><small>Commentaire:</small></td></tr>
-             <tr >
-             <td style='border-bottom: 1px black solid; border-top: 1px black solid; width: 100%' > <?php echo  $command->devis__note_interne ?> </td>
-            </tr>
-     </table>
-
-
-     <div style=" width: 100%; position: absolute; bottom:5%">
-    
-   
-    <table CELLSPACING=0 style=" width: 100%; margin-top: 5px; margin-bottom: 15px;">
+             else{
+                 echo "<small>livraison & facturation : </small><strong><br>";
+                 echo Pdfunctions::showSociete($clientView)  ."</strong>";
+             }
+ 
+          } 
+          if ($command->cmd__code_cmd_client) 
+          {
+             echo "<br> Code cmd: " . $command->cmd__code_cmd_client ;
+          }
+          ?>
+          </strong>
+             </td>
+          </tr>
+      </table>
+ 
+ 
+      <table CELLSPACING=0 style="width: 100%;  margin-top: 80px; ">
+              <tr style=" margin-top : 50px; background-color: #dedede;">
+                 <td style="width: 22%; text-align: left;">Presta<br>Type<br>Gar.</td>
+                 <td style="width: 50%; text-align: left">Ref Tech<br>Désignation Client<br>Complement techniques</td>
+                 <td style="text-align: center; width: 8%"><strong>CMD</strong></td>
+                 <td style="text-align: center; width: 9%"><strong>Livré</strong></td>
+              </tr> 
+              <?php
+              foreach ($commandLignes as $item) {
+                 if($item->cmdl__garantie_option > $item->devl__mois_garantie) 
+                 {
+                   $temp = $item->cmdl__garantie_option ;
+                 }
+                  else 
+                  { 
+                      if (!empty($item->devl__mois_garantie)) 
+                      {
+                         $temp = $item->devl__mois_garantie;
+                      }
+                      else
+                      {
+                         $temp = "";
+                      }
+                    
+                  }
+ 
+                
+ 
+                 echo "<tr style='font-size: 100%;>
+                         <td style='border-bottom: 1px #ccc solid'> ". $item->prestaLib." <br> " .$item->kw__lib ." <br> " . $temp ." mois</td>
+                         <td style='border-bottom: 1px #ccc solid; width: 55%;'> 
+                             <br> <small>désignation :</small> <b>" . $item->devl__designation ."</b><br>"
+                             .$item->famille__lib. " " . $item->marque . " " .$item->modele. " ". $item->devl__modele  ." " .$item->devl__note_interne . 
+                         "</td>
+                          <td style='border-bottom: 1px #ccc solid; text-align: center'><strong> "  . $item->devl_quantite. " </strong></td>
+                          <td style='border-bottom: 1px #ccc solid; border-left: 1px #ccc solid; text-align: right'><strong>  </strong></td>
+                       </tr>";
+              }
+              ?>
+      </table> 
       
-    </table>
-
-    <table style=" margin-top: 10px; color: #8c8c8c; width: 100%;">
-        <tr >
-            <td  style="text-align: center; font-size: 80%; width: 100%;"><br><small>New Eurocomputer-TVA FR33b 397 934 068 Siret 397 934 068 00016 - APE9511Z - SAS au capital 38112.25 €<br>
-            <strong>RECODE by eurocomputeur - 112 allée François Coli -06210 Mandelieu</strong></small></td>
-        </tr>
-    </table>  
-    </div>  
-
-</page>
-
-<?php
-$content = ob_get_contents();
+      <table style=" margin-top: 50px; width: 100%">
+              <tr style=" margin-top: 200px; width: 100%"><td><small>Commentaire:</small></td></tr>
+              <tr >
+              <td style='border-bottom: 1px black solid; border-top: 1px black solid; width: 100%' > <?php echo  $command->devis__note_interne ?> </td>
+             </tr>
+      </table>
+ 
+ 
+      <div style=" width: 100%; position: absolute; bottom:1px">
+     
+    
+      <table CELLSPACING=0 style=" width: 100%;  ">
+         <tr style="background-color: #dedede;">
+                     <td style="text-align: center; width: 30%"><strong>Traitement en atelier </strong></td>
+                     <td style="text-align: center; width: 40%"><strong>Réceptionné par : </strong></td>
+                     <td style="text-align: center; width: 30%"><strong>POIDS</strong></td>
+         </tr> 
+         <tr>
+             <td style="border: 1px #ccc solid; height: 150px;">
+                 
+             </td>
+             <td style="border: 1px #ccc solid; ">
+                 <small><i>Nom/signature/tampon</i></small>
+             </td>
+             <td style="border: 1px #ccc solid; ">
+                 
+             </td>
+         </tr>
+     </table>  
+     
+     </div>  
+ 
+ </page>
+ 
+ <?php
+ $content = ob_get_contents();
 
 try 
 {
@@ -913,7 +991,10 @@ try
     $doc->pdf->SetDisplayMode('fullpage');
     $doc->writeHTML($content);
     ob_clean();
-    $doc->output('C:\laragon\www\fichesTravail\Ft_'.$command->devis__id.'.pdf' , 'F'); 
+    if ($_SERVER['HTTP_HOST'] != "localhost:8080") 
+    {
+      $doc->output('O:\intranet\Auto_Print\FT\Ft_'.$command->devis__id.'.pdf' , 'F'); 
+    }
 } 
 catch (Html2PdfException $e) 
 {
