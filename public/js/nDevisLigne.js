5516d13ce9b7d86cf7a9d2631799cae1807890ac
anthnoybs <anthonybs.pro@gmail.com>
2020-11-01 20:14:04 +0100

    push

---
+++ public/js/nDevisLigne.js
@@ -0,0 +1,102 @@
+
+
+
+$(document).ready(function() {
+
+   
+    
+    //init du commentaire Client global : 
+    if ($('#comClient').length) 
+    {
+        ClassicEditor       
+        .create( document.querySelector( '#comClient' ) ,{
+            fontColor: 
+            {
+                colors: 
+                [
+                    {
+                        color: 'black',
+                        label: 'Black'
+                    },
+                    {
+                        color: 'red',
+                        label: 'Red'
+                    },
+                    {
+                        color: 'DarkGreen',
+                        label: 'Green'
+                    },
+                    {
+                        color: 'Gold',
+                        label: 'Yellow'
+                    },
+                    {
+                        color: 'Blue',
+                        label: 'Blue',
+                    },
+                ]
+            },
+            toolbar: 
+            [
+                'heading', '|',  'bold', 'italic', 'bulletedList', 'numberedList' , 'link', '|', 'undo' , 'redo' , 'fontColor'
+            ]
+             })
+             .catch( error =>
+             {
+                 console.error( error );
+             });     
+    }
+
+
+    if ($('#comInterne').length) 
+    {
+        ClassicEditor       
+        .create( document.querySelector( '#comInterne' ) ,{
+            fontColor: 
+            {
+                colors: 
+                [
+                    {
+                        color: 'black',
+                        label: 'Black'
+                    },
+                    {
+                        color: 'red',
+                        label: 'Red'
+                    },
+                    {
+                        color: 'DarkGreen',
+                        label: 'Green'
+                    },
+                    {
+                        color: 'Gold',
+                        label: 'Yellow'
+                    },
+                    {
+                        color: 'Blue',
+                        label: 'Blue',
+                    },
+                ]
+            },
+            toolbar: 
+            [
+                'heading', '|',  'bold', 'italic', 'bulletedList', 'numberedList' , 'link', '|', 'undo' , 'redo' , 'fontColor'
+            ]
+             })
+             .catch( error =>
+             {
+                 console.error( error );
+             });     
+    }
+    
+    
+    
+   
+   
+    
+    
+    
+    })
+    
+    
+    
