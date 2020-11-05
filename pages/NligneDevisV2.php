00a9022c123e6f9edc40f2ff0d046dc24ebb49df
Antho <anthonybs.pro@gmail.com>
2020-11-05 09:52:11 +0100

    push

--- pages/NligneDevisV2.php
+++ pages/NligneDevisV2.php
@@ -20,11 +20,12 @@ session_start();
  $Article = new App\Tables\Article($Database);
  $Devis = new App\Tables\Devis($Database);
  $Cmd = new App\Tables\Cmd($Database);
+ $General = new App\Tables\General($Database);
  $Database->DbConnect();
 
 
-
-if (!empty($_POST['clientSelect'])) 
+//creation devis :
+if (!empty($_POST['clientSelect']) && empty($_POST['modifReturn'])) 
 {
     switch ($_POST['clientSelect']) 
     {
@@ -67,6 +68,51 @@ if (!empty($_POST['clientSelect']))
     } 
 }
 
+//modification de devis:
+if (!empty($_POST['modifReturn'])) 
+{
+    //creation de Devis:
+    $date = date("Y-m-d H:i:s");
+    //livraison
+    if ($_POST['clientLivraison'] != 'Aucun')
+    { $livraison = $_POST['clientLivraison'];}
+    else { $livraison =  $_POST['clientSelect'];}
+    //contacts
+    if ($_POST['contactSelect'] != 'Aucun')
+    { $_POST['contactSelect'] = $_POST['contactSelect'];}
+    else { $_POST['contactSelect'] =  null;}
+    //contacts livraison
+    if ($_POST['contactLivraison'] != 'Aucun')
+    { $_POST['contactLivraison'] = $_POST['contactLivraison'];}
+    else { $_POST['contactLivraison'] =  null;}
+    //tva 
+    $tva = $Client->getOne($livraison);
+    $tva = $tva->client__tva;
+    //user:
+    $user = $_SESSION['user']->id_utilisateur;
+    //titre : 
+    if (!empty($_POST['titreDevis'])) 
+    {$accents = array('/[áàâãªäÁÀÂÃÄ]/u'=>'a','/[ÍÌÎÏíìîï]/u'=>'i','/[éèêëÉÈÊË]/u'=>'e','/[óòôõºöÓÒÔÕÖ]/u'=>'o','/[úùûüÚÙÛÜ]/u'=>'u','/[çÇ]/u' =>'c');
+        $titre = preg_replace(array_keys($accents), array_values($accents), $_POST['titreDevis']); $titre  = strtoupper($titre);$titre = preg_replace('/([^.a-z0-9]+)/i', '-', $titre);
+    }else {$titre = '' ;}
+
+    $General->updateAll('cmd' , $_POST['clientSelect'] , 'cmd__client__id_fact' , 'cmd__id' , $_POST['modifReturn']);
+    $General->updateAll('cmd' , $livraison , 'cmd__client__id_livr' , 'cmd__id' , $_POST['modifReturn']);
+    $General->updateAll('cmd' , $_POST['contactSelect'] , 'cmd__contact__id_fact' , 'cmd__id' , $_POST['modifReturn']);
+    $General->updateAll('cmd' , $_POST['contactLivraison'] , 'cmd__contact__id_livr' , 'cmd__id' , $_POST['modifReturn']);
+    $General->updateAll('cmd' , $_POST['globalComClient'] , 'cmd__note_client' , 'cmd__id' , $_POST['modifReturn']);
+    $General->updateAll('cmd' , $_POST['globalComInt'] , 'cmd__note_interne' , 'cmd__id' , $_POST['modifReturn']);
+    $General->updateAll('cmd' , $_POST['modele'] , 'cmd__modele_devis' , 'cmd__id' , $_POST['modifReturn']);
+    $General->updateAll('cmd' , $tva , 'cmd__tva' , 'cmd__id' , $_POST['modifReturn']);
+    $General->updateAll('cmd' , $titre , 'cmd__nom_devis' , 'cmd__id' , $_POST['modifReturn']);
+    $General->updateAll('cmd' , $_POST['code'] , 'cmd__code_cmd_client' , 'cmd__id' , $_POST['modifReturn']);
+    $idDevis = $_POST['modifReturn'];
+} 
+
+
+
+
+// creation lignes : 
 if (!empty($_POST['devis'])) 
 {
     $newLines = $Devis->insertLine(
