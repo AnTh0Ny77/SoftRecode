110023930cbd619c3b256165ed3506371198deb0
anthnoybs <anthonybs.pro@gmail.com>
2020-12-09 19:42:17 +0100

    select first

--- public/js/client.js
+++ public/js/client.js
@@ -513,7 +513,7 @@ idUtilisateur = $('#idUtilisateur').val();
         // requete Ajax sur le devis selectionné dans la page mes devis : 
         $.ajax({
             type: 'post',
-            url: "AjaxVisio",
+            url: "AjaxVisio2",
             data : 
             {
                 "AjaxDevis" : dataRow[0]
@@ -522,7 +522,7 @@ idUtilisateur = $('#idUtilisateur').val();
                 dataSet = JSON.parse(data);
                 checkradio(dataSet);
                 $('#loaderPdf').hide();
-                $('#iframeDevis').attr('src', 'pages/ajax/'+idUtilisateur+'devis.html');
+                $('#iframeDevis').attr('src', 'pages/ajax/' + idUtilisateur + 'devis.pdf');
                 $('#iframeDevis').show();
                 $('.multiButton').removeAttr('disabled');
 
