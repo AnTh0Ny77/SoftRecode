fb08629c662ae94c371302bdc137952f04c65a54
Antho <anthonybs.pro@gmail.com>
2021-02-04 16:03:13 +0100

    hello world

--- app/Tables/Client.php
+++ app/Tables/Client.php
@@ -36,7 +36,7 @@ class Client extends Table
 
     public function getOne($id)
     {
-        $request = $this->Db->Pdo->query("SELECT LPAD(client__id,6,0) as client__id,  client__societe , client__adr1 , client__adr2, client__cp , client__ville , client__tel , client__tva_intracom , client__id_vendeur , client__comment , client__tva , client__date_crea, client__dt_last_modif , client__memo_config , client__pays FROM " . $this->Table . " WHERE client__id = " . $id . "");
+        $request = $this->Db->Pdo->query("SELECT LPAD(client__id,6,0) as client__id,  client__societe , client__adr1 , client__adr2, client__cp , client__ville , client__tel , client__tva_intracom , client__id_vendeur , client__fax, client__comment , client__tva , client__date_crea, client__dt_last_modif , client__memo_config , client__pays FROM " . $this->Table . " WHERE client__id = " . $id . "");
         $data = $request->fetch(PDO::FETCH_OBJ);
         return $data;
     }
