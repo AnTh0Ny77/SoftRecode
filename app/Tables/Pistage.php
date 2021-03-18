6aeaf12ee8ce40c569da15fed2d25974757425bc
Antho <anthonybs.pro@gmail.com>
2021-03-18 17:35:06 +0100

    check check

--- app/Tables/Pistage.php
+++ app/Tables/Pistage.php
@@ -16,7 +16,7 @@ class Pistage extends Table
     $this->Db = $db;
   }
 
-  public function addPiste($user, $dateTime , $cmd , $action)
+  public function addPiste($user, $dateTime , $cmd , $action) : bool
   {
     $request = $this->Db->Pdo->prepare('INSERT INTO pistage (pist__id__user , pist__dt , pist__id__cmd , pist__text )
     VALUES (:user, :dt, :cmd, :texte )');
@@ -28,11 +28,41 @@ class Pistage extends Table
    return true;
   }
 
-  public function get_last_pistes()
+  public function get_last_pistes() : array 
   {
-    $request = $this->Db->Pdo->query('SELECT  * FROM pistage  ORDER BY pist__dt DESC LIMIT 50 ');
+    $request = $this->Db->Pdo->query('SELECT  pist__id__user ,  pist__dt , pist__id__cmd , pist__text , u.nom , u.prenom  FROM pistage  
+    LEFT JOIN utilisateur as u ON u.id_utilisateur =  pist__id__user
+    ORDER BY pist__dt DESC LIMIT 50 ');
     $data = $request->fetchAll(PDO::FETCH_OBJ);
     return $data;
+  }
+
+  public function get_pistes_filtres(array $array_filter) : string 
+  {
+    $sql_clause = 'WHERE';
+    $count = 0 ;
+    foreach ($array_filter as $filter) 
+    {
+      $count ++;
+       if ($count > 1 ) 
+       {
+              $sql_clause .= ' AND ';
+       }
+      switch ($filter) 
+      {
+        case (strlen($filter) == 2 and ctype_digit($filter)):
+          //c'est un id user: 
+          $sql_clause .= ' '.$filter.' = pist__id__user';
+          break;
+
+        default:
+          //c'est un client ou une commande : 
+          $sql_clause .= ' '.$filter.' = pist__id__cmd';
+          break;
+      }
+    }
+    
+    return $sql_clause;
   }
 
 
