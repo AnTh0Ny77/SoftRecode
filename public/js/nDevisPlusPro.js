253964d03dce489e16d6c66d581de218859604ab
Anthnoybs <anthonybs.pro@gmail.com>
2021-03-25 17:06:20 +0100

    commit for end off the day

--- public/js/nDevisPlusPro.js
+++ public/js/nDevisPlusPro.js
@@ -8,88 +8,88 @@ $(document).ready(function() {
       })
 
 //init du commentaire Interne global : 
-if ($('#globalComInt').length) 
-{
-    ClassicEditor       
-    .create( document.querySelector( '#globalComInt' ) ,{
-        fontColor: 
-        {
-            colors: 
-            [
-                {
-                    color: 'black',
-                    label: 'Black'
-                },
-                {
-                    color: 'red',
-                    label: 'Red'
-                },
-                {
-                    color: 'DarkGreen',
-                    label: 'Green'
-                },
-                {
-                    color: 'Gold',
-                    label: 'Yellow'
-                },
-                {
-                    color: 'Blue',
-                    label: 'Blue',
-                },
-            ]
-        },
-        toolbar: 
-        [
-            'heading', '|',  'bold', 'italic', 'bulletedList', 'numberedList' , 'link', '|', 'undo' , 'redo' , 'fontColor'
-        ]
-         })
-         .catch( error =>
-         {
-             console.error( error );
-         });     
-}
+// if ($('#globalComInt').length) 
+// {
+//     ClassicEditor       
+//     .create( document.querySelector( '#globalComInt' ) ,{
+//         fontColor: 
+//         {
+//             colors: 
+//             [
+//                 {
+//                     color: 'black',
+//                     label: 'Black'
+//                 },
+//                 {
+//                     color: 'red',
+//                     label: 'Red'
+//                 },
+//                 {
+//                     color: 'DarkGreen',
+//                     label: 'Green'
+//                 },
+//                 {
+//                     color: 'Gold',
+//                     label: 'Yellow'
+//                 },
+//                 {
+//                     color: 'Blue',
+//                     label: 'Blue',
+//                 },
+//             ]
+//         },
+//         toolbar: 
+//         [
+//             'heading', '|',  'bold', 'italic', 'bulletedList', 'numberedList' , 'link', '|', 'undo' , 'redo' , 'fontColor'
+//         ]
+//          })
+//          .catch( error =>
+//          {
+//              console.error( error );
+//          });     
+// }
 
 //init du commentaire Client global : 
-if ($('#globalComClient').length) 
-{
-    ClassicEditor       
-    .create( document.querySelector( '#globalComClient' ) ,{
-        fontColor: 
-        {
-            colors: 
-            [
-                {
-                    color: 'black',
-                    label: 'Black'
-                },
-                {
-                    color: 'red',
-                    label: 'Red'
-                },
-                {
-                    color: 'DarkGreen',
-                    label: 'Green'
-                },
-                {
-                    color: 'Gold',
-                    label: 'Yellow'
-                },
-                {
-                    color: 'Blue',
-                    label: 'Blue',
-                },
-            ]
-        },
-        toolbar: 
-        [
-            'heading', '|',  'bold', 'italic', 'bulletedList', 'numberedList' , 'link', '|', 'undo' , 'redo' , 'fontColor'
-        ]
-         })
-         .catch( error =>
-         {
-             console.error( error );
-         });     
-}
+// if ($('#globalComClient').length) 
+// {
+//     ClassicEditor       
+//     .create( document.querySelector( '#globalComClient' ) ,{
+//         fontColor: 
+//         {
+//             colors: 
+//             [
+//                 {
+//                     color: 'black',
+//                     label: 'Black'
+//                 },
+//                 {
+//                     color: 'red',
+//                     label: 'Red'
+//                 },
+//                 {
+//                     color: 'DarkGreen',
+//                     label: 'Green'
+//                 },
+//                 {
+//                     color: 'Gold',
+//                     label: 'Yellow'
+//                 },
+//                 {
+//                     color: 'Blue',
+//                     label: 'Blue',
+//                 },
+//             ]
+//         },
+//         toolbar: 
+//         [
+//             'heading', '|',  'bold', 'italic', 'bulletedList', 'numberedList' , 'link', '|', 'undo' , 'redo' , 'fontColor'
+//         ]
+//          })
+//          .catch( error =>
+//          {
+//              console.error( error );
+//          });     
+// }
 //fonction de remplissage des select contact en fonction de leur client : 
 //facturation : 
 $('#clientSelect').on('change', function()
