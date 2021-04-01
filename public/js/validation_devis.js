8d868960efd4ae6e679227a39fc396a60f2c5f58
Anthnoybs <anthonybs.pro@gmail.com>
2021-04-01 17:39:22 +0200

    push

--- public/js/validation_devis.js
+++ public/js/validation_devis.js
@@ -14,4 +14,33 @@ $(document).ready(function()
               })  
         })
 
+        //supprime le border rouge quand on sélectionne la radio box : 
+        $('.radioCmd').on('click' , function()
+        {
+                $(this).parent('div').removeClass('border-danger');     
+        })
+
+        //post le formulaire:  
+        $('.post_form').on('click' , function()
+        {
+                //boolean qui servira à validation du form :
+                let autorization = true; 
+                //verifier toutes les radios-boxs:
+                let count_radio = 0; 
+                let count_article = 0;
+                $('.') 
+                $('.radioCmd').each(function()
+                { 
+                        if ($(this).is(':checked')) count_radio ++;
+                })
+                console.log(count_article , count_radio);
+                if (count_article == count_radio) console.log('tout es check ! ');
+                        
+                
+                
+                //variable qui determine ou on redirige form  AVOIR/FACTURE/VALIDATION CLASSIQUE: 
+
+                //POST le formulaire: 
+        })
+
 })
