097934f6db40f8e4b5280897d246e58b789efc39
Anthnoybs <anthonybs.pro@gmail.com>
2021-08-19 09:06:29 +0200

    pn image fix

--- app/Tables/cmd.php
+++ app/Tables/cmd.php
@@ -1678,38 +1678,50 @@ class Cmd extends Table
   //recupère les lignes liées à un devis:
   public function devisLigne_actif($id)
   {
+
     $request = $this->Db->Pdo->query("SELECT
-  cmdl__cmd__id,
-  cmdl__id as devl__id ,cmdl__prestation as  devl__type, 
-  cmdl__pn as devl__modele,  cmdl__designation as devl__designation,
-  cmdl__etat as devl__etat, LPAD(cmdl__garantie_base,2,0) as devl__mois_garantie,
-  cmdl__qte_cmd as devl_quantite, cmdl__prix_barre as  devl__prix_barre, 
-  cmdl__puht as  devl_puht, cmdl__ordre as devl__ordre , cmdl__id__fmm as id__fmm, 
-  cmdl__note_client as devl__note_client,  cmdl__note_interne as devl__note_interne , 
-  cmdl__garantie_option, cmdl__qte_livr , cmdl__qte_fact, cmdl__garantie_puht , cmdl__note_facture,
-  cmdl__etat_masque, cmdl__image, cmdl__actif ,
-  k.kw__lib , k.kw__value , 
-  f.afmm__famille as famille,
-  f.afmm__modele as modele, f.afmm__image as ligne_image , 
-  k2.kw__lib as prestaLib,
-  k3.kw__info as groupe_famille,
-  k3.kw__lib as famille__lib,
-  a.am__marque as marque, 
-  p.apn__pn_long, p.apn__image
-  FROM cmd_ligne 
-  LEFT JOIN keyword as k ON cmdl__etat = k.kw__value AND k.kw__type = 'letat'
-  LEFT JOIN keyword as k2 ON cmdl__prestation = k2.kw__value AND k2.kw__type = 'pres'
-  LEFT JOIN art_fmm as f ON afmm__id = cmdl__id__fmm
-  LEFT JOIN art_pn as p ON cmdl__pn = p.apn__pn
-  LEFT JOIN keyword as k3 ON f.afmm__famille = k3.kw__value AND k3.kw__type = 'famil'
-  LEFT JOIN art_marque as a ON f.afmm__marque = a.am__id
-  WHERE cmdl__cmd__id = " . $id . " AND cmdl__actif > 0 AND cmdl__sous_ref IS NULL
-  ORDER BY devl__ordre ");
+    cmdl__cmd__id,
+    cmdl__id as devl__id ,cmdl__prestation as  devl__type, 
+    cmdl__pn as devl__modele,  cmdl__designation as devl__designation,
+    cmdl__etat as devl__etat, LPAD(cmdl__garantie_base,2,0) as devl__mois_garantie,
+    cmdl__qte_cmd as devl_quantite, cmdl__prix_barre as  devl__prix_barre, 
+    cmdl__puht as  devl_puht, cmdl__ordre as devl__ordre , cmdl__id__fmm as id__fmm, 
+    cmdl__note_client as devl__note_client,  cmdl__note_interne as devl__note_interne , 
+    cmdl__garantie_option, cmdl__qte_livr , cmdl__qte_fact, cmdl__garantie_puht , cmdl__note_facture,
+    cmdl__etat_masque, cmdl__image, cmdl__actif ,
+    k.kw__lib , k.kw__value , 
+    f.afmm__famille as famille,
+    f.afmm__modele as modele, f.afmm__image as ligne_image , 
+    k2.kw__lib as prestaLib,
+    k3.kw__info as groupe_famille,
+    k3.kw__lib as famille__lib,
+    a.am__marque as marque, 
+    p.apn__pn_long, p.apn__image
+    FROM cmd_ligne 
+    LEFT JOIN keyword as k ON cmdl__etat = k.kw__value AND k.kw__type = 'letat'
+    LEFT JOIN keyword as k2 ON cmdl__prestation = k2.kw__value AND k2.kw__type = 'pres'
+    LEFT JOIN art_fmm as f ON afmm__id = cmdl__id__fmm
+    LEFT JOIN art_pn as p ON cmdl__pn = p.apn__pn
+    LEFT JOIN keyword as k3 ON f.afmm__famille = k3.kw__value AND k3.kw__type = 'famil'
+    LEFT JOIN art_marque as a ON f.afmm__marque = a.am__id
+    WHERE cmdl__cmd__id = " . $id . " AND cmdl__actif > 0 AND cmdl__sous_ref IS NULL
+    ORDER BY devl__ordre ");
 
     $data = $request->fetchAll(PDO::FETCH_OBJ);
-    foreach ($data as $ligne) {
-      if (!empty($ligne->ligne_image)) {
-        $ligne->ligne_image = base64_encode($ligne->ligne_image);
+    foreach ($data as $ligne) 
+    {
+      
+      if (!empty($ligne->ligne_image)) 
+      {
+        if (!empty($ligne->apn__image)) 
+        {
+            $ligne->ligne_image = base64_encode($ligne->apn__image);
+        }
+        else
+        {
+            $ligne->ligne_image = base64_encode($ligne->ligne_image);
+        }
+       
       }
     }
 
