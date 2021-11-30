820574ba02d24b2a28404bbbbf180abc345f77aa
Anthnoybs <anthonybs.pro@gmail.com>
2021-11-30 18:45:03 +0100

    catalogue pn model icon

--- pages/catalogue_pn.php
+++ pages/catalogue_pn.php
@@ -151,8 +151,8 @@ if ($_SESSION['config']['model'] == false)
     $model_list = [];
 
 
-$Totoro = new App\Totoro('euro');
-$Totoro->DbConnect();
+// $Totoro = new App\Totoro('euro');
+// $Totoro->DbConnect();
 
 $temp_pn = [];
 foreach ($pn_list as $key => $pn) 
@@ -160,21 +160,21 @@ foreach ($pn_list as $key => $pn)
     $pn->specs = $Stocks->get_specs_pn_show($pn->apn__pn);
     $pn->apn__image  = base64_encode($pn->apn__image);
    
-    $count_stock = $Stocks->count_from_totoro($Totoro, $pn->apn__pn);
+    // $count_stock = $Stocks->count_from_totoro($Totoro, $pn->apn__pn);
     $date_time = new DateTime($pn->apn__date_modif);
 	$date_time = $date_time->format('d/m/Y');
 	$pn->apn__date_modif = $date_time ; 
       
-    foreach ($count_stock as $count) 
-    {
-        if (intval($count->id_etat == 1  )) 
-            $pn->neuf = $count->ct_etat ;
-        if (intval($count->id_etat == 11  )) 
-            $pn->occasion = $count->ct_etat;
-        if (intval($count->id_etat == 21)) 
-            $pn->hs = $count->ct_etat; 
+    // foreach ($count_stock as $count) 
+    // {
+    //     if (intval($count->id_etat == 1  )) 
+    //         $pn->neuf = $count->ct_etat ;
+    //     if (intval($count->id_etat == 11  )) 
+    //         $pn->occasion = $count->ct_etat;
+    //     if (intval($count->id_etat == 21)) 
+    //         $pn->hs = $count->ct_etat; 
 
-    }
+    // }
 
     $marqueur = false;
 
@@ -207,17 +207,22 @@ foreach ($model_list as $key => $model)
 {
     $model->specs = $Stocks->get_specs_modele_show($model->afmm__id);
     $model->afmm__image = base64_encode($model->afmm__image);
-    $count_stock = $Stocks->count_from_totoro($Totoro,$model->afmm__modele);
 
-    foreach ($count_stock as $count) 
-    {
-        if (intval($count->id_etat == 1  )) 
-            $model->neuf = $count->ct_etat ;
-        if (intval($count->id_etat == 11  )) 
-            $model->occasion = $count->ct_etat;
-        if (intval($count->id_etat == 21)) 
-            $model->hs = $count->ct_etat; 
-    }
+    $date_model = new DateTime($model->afmm__dt_modif);
+    $date_model = $date_model->format('d/m/Y');
+    $model->afmm__dt_modif = $date_model;
+    
+    // $count_stock = $Stocks->count_from_totoro($Totoro,$model->afmm__modele);
+
+    // foreach ($count_stock as $count) 
+    // {
+    //     if (intval($count->id_etat == 1  )) 
+    //         $model->neuf = $count->ct_etat ;
+    //     if (intval($count->id_etat == 11  )) 
+    //         $model->occasion = $count->ct_etat;
+    //     if (intval($count->id_etat == 21)) 
+    //         $model->hs = $count->ct_etat; 
+    // }
 
     $marqueur = false ;
 
