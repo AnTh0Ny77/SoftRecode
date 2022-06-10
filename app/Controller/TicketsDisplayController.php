ef1c16a37b6ae7a2439fac6e72a75bf39d929395
Anthnoybs <anthonybs.pro@gmail.com>
2022-06-10 17:27:45 +0200

    search paufine

--- app/Controller/TicketsDisplayController.php
+++ app/Controller/TicketsDisplayController.php
@@ -91,6 +91,7 @@ class TicketsDisplayController extends BasicController
         if (!empty($_GET['searchTickets'])) {
             //clean the search :
             $_GET['searchTickets'] =  $Ticket->clean($_GET['searchTickets']);
+            $text_results = $_GET['searchTickets'];
             //handle get variable :
             $_GET['StateFilter'] =  ['Cloture' , 'Lus' , 'NonLus'];
             $_GET['AuthorFilte'] = 1 ;
@@ -98,6 +99,9 @@ class TicketsDisplayController extends BasicController
         self::handle_search();
         $results = $Ticket->search_tickets_filters($_SESSION['filters'], $_GET['searchTickets'] , $_SESSION['user']->id_utilisateur);
         $list = $results[0];
+        if (empty($list))
+            $alert_results = true;
+        
         $_SESSION['filters'] = $results[1];
         $filters = $_SESSION['filters'];
         //si un ticket à été cloturé : 
