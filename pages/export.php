c4795e41dd38c7104d70582e75a140fab9f22844
Anthnoybs <anthonybs.pro@gmail.com>
2022-11-07 17:15:21 +0100

    export phillipe

--- pages/export.php
+++ pages/export.php
@@ -3,14 +3,11 @@ require "./vendor/autoload.php";
 require "./App/twigloader.php";
 use App\Methods\Pdfunctions;
 session_start();
-
  //URL bloqué si pas de connexion :
- if (empty($_SESSION['user']->id_utilisateur)) 
- {
+ if (empty($_SESSION['user']->id_utilisateur)){
     header('location: login');
  }
- if ($_SESSION['user']->user__facture_acces < 10 ) 
- {
+ if ($_SESSION['user']->user__facture_acces < 10){
    header('location: noAccess');
  }
 
@@ -35,84 +32,68 @@ session_start();
  $marqueur = intval($marqueur->kw__lib);
  $maxFact = $Cmd->getMaxFacture();
  $minFact = $Cmd->getMinFacture();
-
  $alert = false;
- 
  $devisList = $Cmd->getFromStatusAll('VLD');
  //formatte la date pour l'utilisateur:
-    foreach ($devisList as $devis) 
-    {
+    foreach ($devisList as $devis){
       $devisDate = date_create($devis->cmd__date_fact);
       $date = date_format($devisDate, 'd/m/Y');
       $devis->cmd__date_fact = $date;
     }
 
 //si un export à été envoyé :
-if (!empty($_POST['exportStart']) && !empty($_POST['exportEnd'])) 
-{
+if (!empty($_POST['exportStart']) && !empty($_POST['exportEnd'])){
     $exportArray = $Cmd->ligneXport($_POST['exportStart'],$_POST['exportEnd']);
     $getAllLines = $Cmd->exportFinal($exportArray);
     $Keyword->majMarqueur($_POST['exportEnd']);
-    
     $txt = '';
     
-    foreach ($getAllLines as $key => $value) 
-    {
-        if (!empty($value)) 
-        {
-             $commande = $Cmd->GetById($value[0]->cmdl__cmd__id);
+    foreach ($getAllLines as $key => $value){
+       
+        if (!empty($value)){
+        $commande = $Cmd->GetById($value[0]->cmdl__cmd__id);
         $devisDate = date_create($commande->cmd__date_fact);
         $interval = new DateInterval('P30D');
         $date = date_format($devisDate, 'd/m/Y');
         $commande->cmd__date_fact = $date;
         $echeance = $devisDate->add($interval);
         $echeance = date_format($echeance, 'd/m/Y');
-        
         $lignes = $Cmd->devisLigne($value[0]->cmdl__cmd__id);
         $total = Pdfunctions::totalFacture($commande,$value);
         $libelle = 'F';
         
-        if ($commande->cmd__modele_facture == 'AVR') 
-        {
+        if($commande->cmd__modele_facture == 'AVR'){
             $libelle = 'A';
         }
         $libelle = $commande->devis__id.' '.$commande->client__societe;
         $libelle = substr($libelle, 0 , 28);
         //determine les première ligne par rapport au taux de tva: 
-        if (intval($commande->tva_value)  == 10 ) 
-        {
+        if(intval($commande->tva_value) == 10 ){
             $txt.=  'VE;' . $commande->cmd__id_facture .';'.$commande->cmd__date_fact.';'.$echeance.';'.$libelle.';411'.$commande->client__id.';'. number_format($total[3] , 2 , ',', ' ').'; ;
 VE;'.$commande->cmd__id_facture.';'.$commande->cmd__date_fact.'; ;T.V.A;44571200; ;'.number_format($total[2] , 2 ,',', ' ').'
 ' ;
-        }
-        elseif (intval($commande->tva_value)  == 20) 
-        {
+        }elseif (intval($commande->tva_value) == 20){
             $txt.=  'VE;' . $commande->cmd__id_facture .';'.$commande->cmd__date_fact.';'.$echeance.';'.$libelle.';411'.$commande->client__id.';'.number_format($total[3] , 2 ,  ',' ,  ' ').'; ;
 VE;'.$commande->cmd__id_facture.';'.$commande->cmd__date_fact.'; ;T.V.A;44571101; ;'.number_format($total[2] , 2 ,',', ' ').'
 ' ;
-        }
-        else 
-        {
+        }else{
             $txt.=  'VE;' . $commande->cmd__id_facture .';'.$commande->cmd__date_fact.';'.$echeance.';'.$libelle.';411'.$commande->client__id.';'.number_format($total[3] , 2 , ',' , ' ').'; ;
 ';
         }
-       
-        foreach ($value as $test) 
-        {    
-            if (floatval($test->devl_puht) != 00.00 ) 
-            {
+        foreach ($value as $test){ 
+            $libelle_ligne = "";
+            if ($commande->devis__etat == "VLA"){
+                $libelle_ligne = substr($commande->devis__etat, 32); 
+            }
+           
+            if (floatval($test->devl_puht) != 00.00){
                 $compta = $Cmd->getCompta($test , $commande);
                
-             if (!empty($compta[0])) 
-             {
-                $txt.= 'VE;' . $commande->cmd__id_facture .';'.$commande->cmd__date_fact.'; ;'.$test->devl__type .' '.$test->cmdl__qte_fact.' '.$test->famille.';'.$compta[0]->cpt__compte_quadra.'; ;'.number_format($test->devl_puht * $test->cmdl__qte_fact , 2 , ',' ,' ').'
+             if (!empty($compta[0])){
+                $txt.= 'VE;' . $commande->cmd__id_facture .';'.$commande->cmd__date_fact.'; ;'.$test->devl__type .' '.$test->cmdl__qte_fact.' '.$test->famille. ' ' . $libelle_ligne .';'.$compta[0]->cpt__compte_quadra.'; ;'.number_format($test->devl_puht * $test->cmdl__qte_fact , 2 , ',' ,' ').'
 ';
-             }  
-         
-            if (!empty($compta[1])) 
-            {
-               
-                $txt.= 'VE;' . $commande->cmd__id_facture .';'.$commande->cmd__date_fact.'; ;EXT'.$test->cmdl__qte_fact.' '.$test->famille.';'.$compta[1]->cpt__compte_quadra.'; ;'.number_format($test->cmdl__garantie_puht * $test->cmdl__qte_fact, 2, ',' , ' ').'
+             }if(!empty($compta[1])){
+                $txt.= 'VE;' . $commande->cmd__id_facture .';'.$commande->cmd__date_fact.'; ;EXT'.$test->cmdl__qte_fact.' '.$test->famille.'' . $libelle_ligne .';'.$compta[1]->cpt__compte_quadra.'; ;'.number_format($test->cmdl__garantie_puht * $test->cmdl__qte_fact, 2, ',' , ' ').'
 ';
             }
             }    
