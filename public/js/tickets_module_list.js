081853fbc461d252a9b42e35c786b238551566d3
Anthnoybs <anthonybs.pro@gmail.com>
2022-11-16 15:47:11 +0100

    fix bug

--- public/js/tickets_module_list.js
+++ public/js/tickets_module_list.js
@@ -2,7 +2,7 @@
 
 $('.card-recode-cursor').on('click' , function(){
     let value = $(this).children("input[type='hidden']:first").val();
-    if (value.length  == 4 ) {
+    if (value.length == 4 || value.length == 5) {
         $('#idTicket').val(value);
         if ($('#idTicket').val().length == 5 || $('#idTicket').val().length == 4  ) {
             $('#editTicket').submit();
