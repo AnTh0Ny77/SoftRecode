4264c7b4711533bfaa3e5ad2d734d88a9a8a6642
Anthnoybs <anthonybs.pro@gmail.com>
2023-05-09 17:01:31 +0200

    maj import boutique

--- app/Controller/MyRecodeBoutiqueController.php
+++ app/Controller/MyRecodeBoutiqueController.php
@@ -43,7 +43,27 @@ class MyRecodeBoutiqueController extends BasicController {
 
       
 
-       
+       foreach ($list as $key => $value) {
+        $pn = $Article->get_pn_by_id_api($value->apn__pn);
+        if(!empty($pn)){
+                if (!empty($pn->apn__image)) {
+                    $extension = self::getImageMimeType(base64_decode($pn->apn__image));
+                    file_put_contents('public/img/boutique/' . $pn->apn__pn . '.' . $extension, base64_decode($pn->apn__image));
+                    $name = $pn->apn__pn . '.' . $extension;
+                }
+                $body = [
+                    'sar__model' => $pn->apn__pn_long,
+                    'sar__ref_constructeur' => $pn->apn__pn_long,
+                    'sar__description' => $pn->apn__desc_short,
+                    'sar__marque' => $pn->marque,
+                    'sar__famille' => $pn->fam,
+                    'sar__image' => $name
+                ];
+
+                $response  = $Api->postShopArticle($token, $body);
+        }
+       }
+       die();
 
         return self::$twig->render(
             'display_boutique_myrecode.html.twig',[
@@ -52,5 +72,33 @@ class MyRecodeBoutiqueController extends BasicController {
             ]
         );
     }
+
+    public static function getBytesFromHexString($hexdata)
+    {
+        for ($count = 0; $count < strlen($hexdata); $count += 2)
+            $bytes[] = chr(hexdec(substr($hexdata, $count, 2)));
+
+        return implode($bytes);
+    }
+
+    public static function getImageMimeType($imagedata)
+    {
+        $imagemimetypes = array(
+            "jpeg" => "FFD8",
+            "png" => "89504E470D0A1A0A",
+            "gif" => "474946",
+            "bmp" => "424D",
+            "tiff" => "4949",
+            "tiff" => "4D4D"
+        );
+
+        foreach ($imagemimetypes as $mime => $hexbytes) {
+            $bytes = self::getBytesFromHexString($hexbytes);
+            if (substr($imagedata, 0, strlen($bytes)) == $bytes)
+                return $mime;
+        };
+
+        return null;
+    }  
 
 }
