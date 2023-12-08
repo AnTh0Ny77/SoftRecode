c69cf5b9d5580398d648f3723130f9bf7e4838ca
Anthnoybs <anthonybs.pro@gmail.com>
2023-12-08 10:57:23 +0100

    maj test

--- app/TwigCustomFunction.php
+++ app/TwigCustomFunction.php
@@ -7,7 +7,8 @@ class TwigCustomFunction extends AbstractExtension
     public function getFunctions()
     {
         return [
-            new TwigFunction('boTel', [$this, 'boTel']),
+            new TwigFunction('boTel', [$this, 'boTel'] ),
+            new TwigFunction('ajouterZeros', [$this, 'ajouterZeros']),
         ];
     }
 
@@ -23,6 +24,19 @@ class TwigCustomFunction extends AbstractExtension
             $botel = substr($tel,0,2).".".substr($tel,2,2).".".substr($tel,4,2).".".substr($tel,6,2).".".substr($tel,8,2);
         return $botel; // indique la valeur à renvoyer 
     } 
+
+    public function ajouterZeros($chaine) {
+        $longueurChaine = strlen($chaine);
+        
+        if ($longueurChaine < 6) {
+            $zerosManquants = 6 - $longueurChaine;
+            $chaine = str_repeat('0', $zerosManquants) . $chaine;
+        } elseif ($longueurChaine > 6) {
+            $chaine = substr($chaine, 0, 6); // Si la chaîne est plus longue, on la tronque à 6 caractères
+        }
+    
+        return $chaine;
+    }
 
   
 }
