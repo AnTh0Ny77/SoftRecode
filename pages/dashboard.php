3ff67311106ff76f30809d55408a58605b6a47f6
anthnoybs <anthonybs.pro@gmail.com>
2024-01-11 11:57:59 +0100

    debug prod

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -16,9 +16,22 @@ $_SESSION['user']->devis_cours = $Stats->get_user_devis($_SESSION['user']->id_ut
 $commandes_valides = $Stats->get_commande_valide();
 $devis_envoyes = $Stats->get_devis_quizaine();
 
+// tempo pour modif code sans gener les autres..
+if ($_SESSION['user']->id_utilisateur == '56')
+{
+	echo $twig->render('dashboard_dev.twig', [
+	'user'=>$_SESSION['user'],
+	'commandes_valides' => $commandes_valides,
+	'devis_envoyes' => $devis_envoyes
+]);
+}
 
- echo $twig->render('dashboard.twig', [
-    'user'=>$_SESSION['user'],
-    'commandes_valides' => $commandes_valides,
-    'devis_envoyes' => $devis_envoyes
- ]);
+else
+{
+echo $twig->render('dashboard.twig', [
+	'user'=>$_SESSION['user'],
+	'commandes_valides' => $commandes_valides,
+	'devis_envoyes' => $devis_envoyes
+]);
+}
+
